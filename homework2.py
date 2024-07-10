38655902eb47342c1f8a4bd413c8581394d70200
Strel79 <a.strelins@mail.ru>
2024-07-10 21:34:03 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kolicestvo_vipolnenih_DZ = 12
+kolicestvo_zatracenih_casov = 1.5
+nazvaie_kursa = "Pyton,"
+vremja_na_odno_zadanie = kolicestvo_zatracenih_casov / kolicestvo_vipolnenih_DZ
+print(nazvaie_kursa, " всего задач:", kolicestvo_vipolnenih_DZ, ",", "затрачено часов:", kolicestvo_zatracenih_casov , ", ", "среднее время выполнения", vremja_na_odno_zadanie, "часа.")
